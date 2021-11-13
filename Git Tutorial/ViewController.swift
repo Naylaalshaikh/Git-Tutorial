a982e4949aaefee83af9557f93cf942635a43057
Nayla Alshaikh <201800901@student.polytechnic.bh>
2021-11-13 14:36:45 +0300

    Added message string constant which prints when the view loads.

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let message = "Hello Git!"
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+           super.viewDidLoad()
+           print(message)
+       }
+    
+       override func didReceiveMemoryWarning() {
+           super.didReceiveMemoryWarning()
+           // Dispose of any resources that can be recreated.
+       }
 }
 
